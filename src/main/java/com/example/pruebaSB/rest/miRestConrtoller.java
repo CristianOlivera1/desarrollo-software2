4d5d5249c82d7989947c09d2f0183c98797ad808
SamuelF32 <raulmontesinosvaldivia@gmail.com>
2024-11-05 14:44:29 -0500

    prueba rama

--- src/main/java/com/example/pruebaSB/rest/miRestConrtoller.java
+++ src/main/java/com/example/pruebaSB/rest/miRestConrtoller.java
@@ -3,12 +3,17 @@ package com.example.pruebaSB.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.lang.System.*;
+
 @RestController
 public class miRestConrtoller {
     @GetMapping("/")
 
     public String saludo(){
         return "HOla mundo";
+    }
+    public String respuesta(){
+        return "saludo de respuesta";
     }
 
 }
